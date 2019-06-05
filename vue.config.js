700901c0fc344f7c72210f3da7de427fc586d488
yankun <15635699721@163.com>
2019-06-05 13:23:26 +0800

    用户登录系统开发完毕

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    lintOnSave: true,					// 在保存代码的时候开启eslint代码检查机制
-    devServer: {							// 实时保存、编译的配置段
-      open:true,
-      port: 12306
-    }
+  lintOnSave: true, // 在保存代码的时候开启eslint代码检查机制
+  devServer: {
+    // 实时保存、编译的配置段
+    open: true, // 自动打开浏览器
+    port: 12306 // http服务的端口号码设定
   }
+};
