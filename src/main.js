0afe1ee4656b7b52fbacf0e8cab7484988cd340b
yankun <15635699721@163.com>
2019-06-12 18:52:13 +0800

    用户管理，权限管理开发完全

--- src/main.js
+++ src/main.js
@@ -3,29 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/global.css";
 import "./assets/fonts/iconfont.css";
-import axios from "axios";
-axios.interceptors.request.use(
-  function(config) {
-    // 给axios配置token
-    var token = window.sessionStorage.getItem("token");
-    if (token !== null) {
-      config.headers.Authorization = token;
-    }
-    return config;
-  },
-  function(error) {
-    return Promise.reject(error);
-  }
-);
-// axios配置请求公共根地址
-axios.defaults.baseURL = "http://127.0.0.1:11333/api/private/v1/";
+import axios from "./http";
 // 给axios配置成为Vue的成员
 Vue.prototype.$http = axios;
 import ElementUI from "element-ui";
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
-
+import MyBread from "./components/MyBread.vue";
+Vue.component("my-bread", MyBread);
 new Vue({
   router,
   render: h => h(App)
